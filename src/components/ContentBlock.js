bd707f6b299b7154d707e3304fc3c2ea2ee9c9a3
Luuk Schipperheijn <luuk@luuk.computer>
2023-06-22 03:02:05 +0200

    ditch jquery

--- src/components/ContentBlock.js
+++ src/components/ContentBlock.js
@@ -2,29 +2,14 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { remark } from "remark";
 import remarkHTML from "remark-html";
-import $ from "jquery";
 
 const ContentBlock = ({ label, columns, rows, round, body }) => {
-  let HTMLContent = remark().use(remarkHTML).processSync(body).toString();
-  const $html = $(HTMLContent);
-  $html.find("img").parent().addClass("img-parent");
-  const $container = $("<div />").append($html);
-  $container
-    .find(".img-parent")
-    .wrapAll('<div class="img-parent-container" />');
-
-  HTMLContent = $container.html();
-  console.log({
-    $html: $html,
-    HTMLContent,
-    $container,
-  });
+  const HTMLContent = remark().use(remarkHTML).processSync(body).toString();
   return (
     <>
       <div
-        className={`item content-block-toggle row-${rows} col-${columns} radius-${
-          round ? 2 * Math.min(rows, columns) : 1
-        }`}
+        className={`item content-block-toggle row-${rows} col-${columns} radius-${round ? 2 * Math.min(rows, columns) : 1
+          }`}
       >
         <div className="title">{label}</div>
       </div>
