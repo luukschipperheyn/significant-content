88193cf0842326b5356083a2023b4a104f6c772c
Luuk <luuk@luuk.computer>
2023-06-23 16:43:52 +0200

    seo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const _ = require("lodash");
 const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
-const { graphql } = require("gatsby");
 
 exports.createPages = ({ actions: { createPage }, graphql }) => {
   createPage({
